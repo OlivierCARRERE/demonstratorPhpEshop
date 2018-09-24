edc064297ab7c87a9e1bb553ba011fa0f34f631b
olivier <carrereolivier@yahoo.fr>
2018-09-24 11:25:22 +0200

    index.php created and added.

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
+  <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/main.css">
+
+  <script src="js/libs/modernizr-2.5.0.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+  <div id="topheader">
+    <div class="container">
+      <ul>
+        <li><a href="" title="S'inscrire">S'inscrire</a></li>
+        <li><a href="" title="Se connecter">Se connecter</a></li>
+      </ul>
+    </div>
+  </div>
+  <header>
+    <div class="container">
+      <div id="header">
+       <div class="panier">
+          <a href="panier.php"> Votre Panier :<em> 2 article(s) </em></a> 
+          <span>  <em>2000 €</em></span>
+        </div>
+      </div>
+    </div>
+  </header>
+  <nav>
+    <ul class="container">
+      <li><a href="index.php" title="Accueil">Accueil</a></li>
+      <li><a href="produits.php" title="Produits">Produits</a>
+          <ul>
+            <li><a href="" title="Ecrans">Ecrans</a></li>
+            <li><a href="" title="Ordinateurs">Ordinateurs</a></li> 
+            <li><a href="" title="Tablettes">Tablettes</a></li>
+            <li><a href="" title="Accessoires">Accessoires</a></li>
+          </ul>
+      </li>
+      <li><a href="contact.php" title="Contact">Contact</a></li>  
+    </ul>
+  </nav>
+  <div class="main">
+    <div class="container">
+      <h1>Bienvenue ...</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2>Les Nouveautés</h2>
+      <ul class="produits">
+        <li>
+          <a href="produit.php">
+            <img src="img/produits/mac.png" alt="mac">
+            <h4>Mac 27 pouces</h4>
+          </a>
+          <div class="prix">
+            2500 €
+          </div>
+          <span> <a href="" title="Ajouter au panier">+</a></span>
+        
+        </li>
+       <li>
+          <a href="produit.php">
+            <img src="img/produits/mac.png" alt="mac">
+            <h4>Mac 27 pouces</h4>
+          </a>
+          <div class="prix">
+            2500 €
+          </div>
+          <span> <a href="" title="Ajouter au panier">+</a></span>
+        
+        </li>
+        <li>
+          <a href="produit.php">
+            <img src="img/produits/mac.png" alt="mac">
+            <h4>Mac 27 pouces</h4>
+          </a>
+          <div class="prix">
+            2500 €
+          </div>
+          <span> <a href="" title="Ajouter au panier">+</a></span>
+        
+        </li>
+        <li>
+          <a href="produit.php">
+            <img src="img/produits/mac.png" alt="mac">
+            <h4>Mac 27 pouces</h4>
+          </a>
+          <div class="prix">
+            2500 €
+          </div>
+          <span> <a href="" title="Ajouter au panier">+</a></span>
+        
+        </li>
+        <li>
+          <a href="produit.php">
+            <img src="img/produits/mac.png" alt="mac">
+            <h4>Mac 27 pouces</h4>
+          </a>
+          <div class="prix">
+            2500 €
+          </div>
+          <span> <a href="" title="Ajouter au panier">+</a></span>
+        
+        </li>
+        
+      </ul>
+      <div class="col left charte">
+       
+          <h3>Notre charte</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button><a href="" title="">Lire la suite</a></button>
+       
+      </div>
+      <div class="col right livraison">
+
+          <h3>Nos livraisons</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button><a href="" title="">Voir les conditions</a></button>
+
+      </div>
+
+      <div class="clearfix"></div>
+    </div>
+  </div>
+  <footer>
+   <div class="container">
+ <p> Site réaliser pour la formation "Mini boutique en PHP" - (c) - athakim</p>
+   </div>
+  </footer>
+
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+
+  <script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+</body>
+</html>
